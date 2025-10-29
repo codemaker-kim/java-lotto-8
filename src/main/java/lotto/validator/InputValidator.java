882625d54536042ae7658d3600d09cfc1bde31a3
codemaker-kim <tnals72441@daum.net>
2025-10-29 16:32:05 +0900

    refactor(InputValidator): 역할이 많은 메서드 분할
    - validate() 내부 로직을 메서드 추출하여 분할

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -2,10 +2,18 @@ package lotto.validator;
 
 public class InputValidator {
     public void validate(String input) {
+        validateEmpty(input);
+
+        validateInteger(input);
+    }
+
+    private void validateEmpty(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 구입금액 입력이 비어있습니다.");
         }
+    }
 
+    private void validateInteger(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
