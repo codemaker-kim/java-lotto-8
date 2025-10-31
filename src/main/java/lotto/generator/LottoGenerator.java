5db13d9a17f1528f7a1aeed699a645c0676ef369
codemaker-kim <tnals72441@daum.net>
2025-11-01 03:28:40 +0900

    feat(LottoGenerator): 로또 구입금액이 0원일 경우 예외 처리 추가

--- src/main/java/lotto/generator/LottoGenerator.java
+++ src/main/java/lotto/generator/LottoGenerator.java
@@ -1,6 +1,5 @@
 package lotto.generator;
 
-import static java.math.BigDecimal.ZERO;
 import static lotto.generator.LottoConstant.MAXIMUM_NUMBER;
 import static lotto.generator.LottoConstant.MINIMUM_NUMBER;
 import static lotto.generator.LottoConstant.PRICE;
@@ -8,15 +7,17 @@ import static lotto.generator.LottoConstant.SIZE;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import lotto.Lotto;
+import lotto.domain.Lotto;
 
 public class LottoGenerator {
 
     private final int ZERO = 0;
 
     public List<Lotto> generateLotto(int money) {
-        validateMoney(money);
+        validateMoneyDivisibleByPrice(money);
+        validateMoneyNotZero(money);
 
         List<Lotto> lottos = new ArrayList<>();
 
@@ -28,17 +29,27 @@ public class LottoGenerator {
         return lottos;
     }
 
-    private void validateMoney(int money) {
+    private void validateMoneyDivisibleByPrice(int money) {
         if (money % PRICE.getValue() != ZERO) {
             throw new IllegalArgumentException("[ERROR] 로또 가격이 맞아 떨어지지 않습니다. 로또 하나 가격: " + PRICE.getValue());
         }
     }
 
+    private void validateMoneyNotZero(int money) {
+        if (money == ZERO) {
+            throw new IllegalArgumentException("[ERROR] 로또는 최소한 하나는 구매해야 합니다.");
+        }
+    }
+
     private Lotto createLotto() {
-        return new Lotto(Randoms.pickUniqueNumbersInRange(
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
                 MINIMUM_NUMBER.getValue(),
                 MAXIMUM_NUMBER.getValue(),
                 SIZE.getValue()
-        ));
+        );
+
+        Collections.sort(numbers);
+
+        return new Lotto(numbers);
     }
 }
