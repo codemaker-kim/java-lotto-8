be3bd0d587b2dbb4bddd56948a4f0110f1f17cd7
codemaker-kim <tnals72441@daum.net>
2025-11-01 03:27:44 +0900

    test(LottoGeneratorTest): 로또 구입금액이 0원일 경우의 테스트 케이스 추가

--- src/test/java/lotto/generator/LottoGeneratorTest.java
+++ src/test/java/lotto/generator/LottoGeneratorTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-import lotto.Lotto;
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +14,7 @@ class LottoGeneratorTest {
 
     private static final int ASSERT_LOTTO_SIZE = 5;
     private static final int MINIMUM_LOTTO_PRICE = 1;
+    private static final int ZERO = 0;
     private LottoGenerator generator;
 
     @BeforeEach
@@ -32,6 +33,19 @@ class LottoGeneratorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("로또 가격이 맞아 떨어지지 않습니다.");
     }
+
+    @Test
+    @DisplayName("로또 구입금액이 0원이라면 예외를 발생시킨다.")
+    void moneyInputIsZero() {
+        // given
+        final int money = ZERO;
+
+        // when & then
+        assertThatThrownBy(() -> generator.generateLotto(money))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("로또는 최소한 하나는 구매해야 합니다.");
+    }
+
 
     @Test
     @DisplayName("로또 구입금액을 로또 1개 가격으로 나눈 수만큼의 로또를 생성한다.")
