3124a95d4d80125972e1b5027921f9325d876aa5
codemaker-kim <tnals72441@daum.net>
2025-11-03 19:58:49 +0900

    refactor(AppConfig): 각 클래스에 대한 구현체 반환 메서드 작성

--- src/main/java/lotto/config/AppConfig.java
+++ src/main/java/lotto/config/AppConfig.java
@@ -1,20 +1,50 @@
 package lotto.config;
 
+import lotto.generator.BonusNumberGenerator;
+import lotto.generator.LottoGenerator;
+import lotto.generator.WinningNumberGenerator;
 import lotto.handler.BonusNumberHandler;
 import lotto.handler.LottoHandler;
 import lotto.handler.WinningNumberHandler;
+import lotto.validator.BonusNumberInputValidator;
+import lotto.validator.MoneyInputValidator;
+import lotto.validator.WinningNumberInputValidator;
 
 public class AppConfig {
 
     public LottoHandler lottoHandler() {
-        return new LottoHandler();
+        return new LottoHandler(moneyInputValidator(), lottoGenerator());
     }
 
     public BonusNumberHandler bonusNumberHandler() {
-        return new BonusNumberHandler();
+        return new BonusNumberHandler(bonusNumberInputValidator(), bonusNumberGenerator());
     }
 
     public WinningNumberHandler winningNumberHandler() {
-        return new WinningNumberHandler();
+        return new WinningNumberHandler(winningNumberGenerator(), winningNumberInputValidator());
+    }
+
+    private MoneyInputValidator moneyInputValidator() {
+        return new MoneyInputValidator();
+    }
+
+    private LottoGenerator lottoGenerator() {
+        return new LottoGenerator();
+    }
+
+    private WinningNumberInputValidator winningNumberInputValidator() {
+        return new WinningNumberInputValidator();
+    }
+
+    private WinningNumberGenerator winningNumberGenerator() {
+        return new WinningNumberGenerator();
+    }
+
+    private BonusNumberInputValidator bonusNumberInputValidator() {
+        return new BonusNumberInputValidator();
+    }
+
+    private BonusNumberGenerator bonusNumberGenerator() {
+        return new BonusNumberGenerator();
     }
 }
