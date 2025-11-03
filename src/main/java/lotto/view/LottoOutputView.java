bb12d8473d629462a80aff0e68513e4b316573e6
codemaker-kim <tnals72441@daum.net>
2025-11-03 19:30:42 +0900

    feat(LottoOutputView): 수익률에 대한 계산 출력

--- src/main/java/lotto/view/LottoOutputView.java
+++ src/main/java/lotto/view/LottoOutputView.java
@@ -5,18 +5,18 @@ import lotto.calculator.WinningPrize;
 import lotto.view.dto.LottoStringDto;
 
 public class LottoOutputView {
-    public void lottoCountView(int lottoCount) {
+    public static void lottoCountView(int lottoCount) {
         String message = String.format("%d개를 구매했습니다.", lottoCount);
 
         System.out.println(message);
     }
 
-    public void lottoView(LottoStringDto dto) {
+    public static void lottoView(LottoStringDto dto) {
         dto.lottoNumbers()
                 .forEach(System.out::println);
     }
 
-    public void statisticView(final Map<WinningPrize, Integer> winningPrizes) {
+    public static void statisticView(final Map<WinningPrize, Integer> winningPrizes) {
         System.out.println("당첨 통계\n---");
 
         for (WinningPrize prize : WinningPrize.values()) {
@@ -24,12 +24,18 @@ public class LottoOutputView {
         }
     }
 
-    private void printWinningStatistic(WinningPrize prize, int count) {
+    private static void printWinningStatistic(WinningPrize prize, int count) {
         String message = String.format("%d개 일치 (%d원) - %d개",
                 prize.getMatchCount(),
                 prize.getPrize(),
                 count
         );
+
+        System.out.println(message);
+    }
+
+    public static void rateView(double rate) {
+        String message = String.format("총 수익률은 %.2f\\%입니다.", rate);
 
         System.out.println(message);
     }
