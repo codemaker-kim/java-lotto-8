820a9cbf6a2de21ee3aa2b944751e4eefbce58ef
codemaker-kim <tnals72441@daum.net>
2025-11-03 10:49:49 +0900

    test(BonusNumberValidatorTest): 보너스 번호 입력 예외 테스트 케이스 작성

---
+++ src/test/java/lotto/validator/BonusNumberInputValidatorTest.java
@@ -0,0 +1,47 @@
+package lotto.validator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BonusNumberInputValidatorTest {
+    private BonusNumberInputValidator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = new BonusNumberInputValidator();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("보너스 번호 입력이 비어있을 경우, 예외를 발생시킨다.")
+    void emptyNumberInput(String input) {
+        // assert
+        assertThatThrownBy(() -> validator.validate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("보너스 번호 입력이 비어있습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("보너스 번호 입력이 숫자가 아닐 경우, 예외를 발생시킨다.")
+    @ValueSource(strings = {
+            ",,,,",
+            "abasdf",
+            "!@#$%",
+            "132,45",
+            "12,345"
+    })
+    void notNumericNumberInput(String invalidInput) {
+        // given
+        final String input = invalidInput;
+
+        // when & then
+        assertThatThrownBy(() -> validator.validate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("보너스 번호 입력에는 숫자만 입력 가능합니다.");
+    }
+}
