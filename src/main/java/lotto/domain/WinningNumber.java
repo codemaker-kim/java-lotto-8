1ec8734cefa7edf48740de132da328eadc1c0a95
codemaker-kim <tnals72441@daum.net>
2025-11-03 21:39:56 +0900

    refactor(WinningNumber): 너무 긴 검증 메서드 역할 분리

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -1,7 +1,10 @@
 package lotto.domain;
 
+import static lotto.global.LottoConstant.MAXIMUM_NUMBER;
+import static lotto.global.LottoConstant.MINIMUM_NUMBER;
+import static lotto.global.LottoConstant.SIZE;
+
 import java.util.List;
-import lotto.global.LottoConstant;
 
 public class WinningNumber {
 
@@ -13,23 +16,31 @@ public class WinningNumber {
     }
 
     private void validate(List<Integer> numbers) {
-        int numbersSize = numbers.size();
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
+    }
 
-        if (numbersSize != 6) {
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != SIZE.getValue()) {
             throw new IllegalArgumentException("[ERROR] 당첨번호는 6개여야 합니다.");
         }
+    }
 
+    private void validateDuplicate(List<Integer> numbers) {
         long distinctCount = numbers.stream()
                 .distinct()
                 .count();
 
-        if (distinctCount != numbersSize) {
+        if (distinctCount != numbers.size()) {
             throw new IllegalArgumentException("[ERROR] 당첨번호에 중복되는 숫자가 존재합니다.");
         }
+    }
 
+    private void validateRange(List<Integer> numbers) {
         boolean isOverLottoRange = numbers.stream()
-                .anyMatch(number -> number < LottoConstant.MINIMUM_NUMBER.getValue()
-                        || number > LottoConstant.MAXIMUM_NUMBER.getValue());
+                .anyMatch(number -> number < MINIMUM_NUMBER.getValue()
+                        || number > MAXIMUM_NUMBER.getValue());
 
         if (isOverLottoRange) {
             throw new IllegalStateException("[ERROR] 당첨 번호 내의 숫자가 로또 범위를 초과합니다.");
