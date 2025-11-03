56d1ce3cad801645fb0dae9cfd3c9c7eb285ac20
codemaker-kim <tnals72441@daum.net>
2025-11-03 16:38:14 +0900

    test(WinningStatisticCalculatorTest): 당첨 통계 계산 테스트 작성

---
+++ src/test/java/lotto/calculator/WinningStatisticCalculatorTest.java
@@ -0,0 +1,77 @@
+package lotto.calculator;
+
+import static lotto.calculator.WinningPrize.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.calculator.dto.LottosDto;
+import lotto.calculator.dto.WinningInfoDto;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
+import lotto.domain.WinningNumber;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningStatisticCalculatorTest {
+
+    private WinningStatisticCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new WinningStatisticCalculator(
+                new WinningNumber(List.of(1, 2, 3, 4, 5, 6)),
+                new BonusNumber(7)
+        );
+    }
+
+    @Test
+    @DisplayName("당첨 여부를 올바르게 판별한다.")
+    void calculateWinning() {
+        // given
+        LottosDto dto = new LottosDto(createLottoList());
+
+        // when
+        WinningInfoDto result = calculator.calculateWinningStatistic(dto);
+
+        // then
+        assertThat(result.winningPrizes()
+                .get(FIRST))
+                .isEqualTo(1);
+        assertThat(result.winningPrizes()
+                .get(SECOND))
+                .isEqualTo(1);
+        assertThat(result.winningPrizes()
+                .get(THIRD))
+                .isEqualTo(0);
+        assertThat(result.winningPrizes()
+                .get(FOURTH))
+                .isEqualTo(1);
+        assertThat(result.winningPrizes()
+                .get(FIFTH))
+                .isEqualTo(1);
+
+        assertThat(result.winningRate().doubleValue()).isEqualTo(507513.75);
+    }
+
+    private List<Lotto> createLottoList() {
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            lottoList.add(createLotto(i));
+        }
+        return lottoList;
+    }
+
+    private Lotto createLotto(int offset) {
+        return new Lotto(List.of(
+                1 + offset,
+                2 + offset,
+                3 + offset,
+                4 + offset,
+                5 + offset,
+                6 + offset
+        ));
+    }
+}
